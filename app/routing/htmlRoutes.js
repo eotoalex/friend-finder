23b448b469b257c0f60fec1d9511a567f35e964c
Alex Puello <apuello88@yahoo.com>
2019-10-31 06:52:20 -0400

    Redid the whole set up of files.

---
+++ app/routing/htmlRoutes.js
@@ -0,0 +1,10 @@
+
+var friends = require('../data/friends.js');
+var test = require('./htmlRoutes.js');
+var fs = require('fs');
+var express = require('express');
+var path = require ('path');
+
+var PORT = process.env.PORT || 3000;
+
+console.log(friends);
