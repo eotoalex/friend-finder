297b4e956e07850e48c85a73ce7fc56516679df3
Alex Puello <apuello88@yahoo.com>
2019-10-31 14:37:40 -0400

    test

--- server.js
+++ server.js
@@ -1,12 +1,18 @@
 // var friends = require('../app/data/friends.js');
 // var test = require('./htmlRoutes.js');
 // var fs = require('fs');
-// var express = require('express');
-// var path = require ('path');
+var express = require('express');
+var path = require ('path');
 
-// var PORT = process.env.PORT || 3000;
+const app = express();
+
+var PORT = process.env.PORT || 3000;
 
 // console.log(friends);
 
 
 console.log('test');
+
+app.listen(PORT,)
+
+
