54223ca3fbcc23a402add2546b89c18592b85489
Alex Puello <apuello88@yahoo.com>
2019-11-05 22:08:26 -0500

    removing favicon to check to see if this fixes the heroku bug which is making it not allow my survey page to open.

--- server.js
+++ server.js
@@ -1,11 +1,8 @@
-// var friends = require('../app/data/friends.js');
-// var test = require('./htmlRoutes.js');
-// var fs = require('fs');
 var express = require('express');
 var path = require ('path');
 var routes = require('./app/routing/apiRoutes.js');
 var htmlRoutes = require('./app/routing/htmlRoutes')
-var friends = require('./app/data/friends');
+
 
 const app = express();
 
