67f17b6f9985e5df1d84ebada825132c8c91b713
Alex Puello <apuello88@yahoo.com>
2020-01-01 22:45:45 -0500

    cleaned up the code in the routing folder.

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -13,82 +13,58 @@ Router.get('/friends', function(req,res){
 
 Router.post('/friends',urlencodedParser,function(req,res){
 
-var newUser = req.body;
-var friendsData = friends.friends;
+    var newUser = req.body;
+    var friendsData = friends.friends;
 
-friendsData.push(req.body);
+    friendsData.push(req.body);
 
-
-
-// This sends the response object back to the user to see who they matched with.
-res.json(checkingForMatches(friendsData,newUser));
-
-
+    // This sends the response object back to the user to see who they matched with.
+    res.json(checkingForMatches(friendsData,newUser));
 });
 
 var checkingForMatches = function (friendsObj,usr){
-    
     var usrObj = usr;
     var matchList = [];
     
     for (var i = 0; i <= friendsObj.length - 1; i++){
         var result = 0;
-
         for (var j = 0; j < 10; j++){
             if (parseFloat(usrObj.scores[j]) > friendsObj[i].scores[j]){
-            result += (parseFloat(usrObj.scores[j]) - friendsObj[i].scores[j]);}
-                else if(parseFloat(usrObj.scores[j]) < friendsObj[i].scores[j]){
-                    result += friendsObj[i].scores[j] - parseFloat(usrObj.scores[j])
-                }  
+            result += (parseFloat(usrObj.scores[j]) - friendsObj[i].scores[j]);
+            } else if(parseFloat(usrObj.scores[j]) < friendsObj[i].scores[j]){
+                result += friendsObj[i].scores[j] - parseFloat(usrObj.scores[j]);
+              }  
         }
         if(friendsObj[i].name !== usrObj.name){
-
-        matchList.push({
-            name:friendsObj[i].name,
-            photo:friendsObj[i].photo,
-            diff:result,
-           
-        });      
-    }  
-  
-    }
+            matchList.push({
+                name:friendsObj[i].name,
+                photo:friendsObj[i].photo,
+                diff:result
+            });      
+        }  
+    };
     var postResponse = sumArr(matchList);
     return postResponse;
     
-           
-        //    This function has to take in the object and compare the diff values between friends.
-            function sumArr (obj){
-                console.log(obj);
-                var newArr = []; 
-                for(var i = 0; i < obj.length; i++){
-                    newArr.push(obj[i].diff);
+    //This function has to take in the object and compare the diff values between friends.
+    function sumArr (obj){
+        console.log(obj);
+        var newArr = []; 
+        for(var i = 0; i < obj.length; i++){
+            newArr.push(obj[i].diff);
+        };
+        var smllNum = Math.min(...newArr);
+        for(var j = 0; j < obj.length; j++){
+            if(obj[j].diff === smllNum){
+                var match = {
+                    name:obj[j].name,
+                    photo:obj[j].photo,  
                 }
-                var smllNum = Math.min(...newArr);
-                for(var j = 0; j < obj.length; j++){
-                    if(obj[j].diff === smllNum){
-                        
-                        
-
-                        var match = {
-                            name:obj[j].name,
-                            photo:obj[j].photo,
-                            
-                        }
-
-                       
-                        
-                      return match;
-                    }
-                    
-                }
-               
-              
-               
-            };
-            
-            
-   
-}
+                return match;
+            }   
+        }; 
+    };  
+};
 
 
 
--- server.js
+++ server.js
@@ -2,13 +2,8 @@ var express = require('express');
 var path = require ('path');
 var routes = require('./app/routing/apiRoutes.js');
 var htmlRoutes = require('./app/routing/htmlRoutes.js')
-
-
 const app = express();
-
 var PORT = process.env.PORT || 3000;
-
-
 // This api route leads to the friends object information.
 app.use('/api', routes );
 // This routes will check for user input after the initial slash to ensure that their entry is recognizable, if not the user is routed back to the home page by default.
@@ -21,14 +16,10 @@ app.get("/", function(req, res) {
 app.use("/", htmlRoutes);
 
   // This is the default setting for the web page.
-  app.get('/:id', function(req, res){
-    res.sendFile(path.join(__dirname, "/app/public/home.html"));
+app.get('/:id', function(req, res){
+  res.sendFile(path.join(__dirname, "/app/public/home.html"));
 
-  });
-  
-
-
-
+});
 
 app.listen(PORT, function(){
     console.log('Listening on PORT: ' + PORT);
